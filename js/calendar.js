08ee43ac9971a98ffe3e7737a89f944c5420d8cf
noel <antonin.noel@genesio.fr>
2015-04-03 00:18:22 +0200

    implement popup

--- js/calendar.js
+++ js/calendar.js
@@ -1,18 +1,24 @@
-$('.popover-markup > .trigger').popover({
-    html : true,
-    title: function() {
-      return $(".container").find('.head').html();
-    },
-    content: function() {
-      return $(".container").find('.content').html();
-    },
-    container: 'body',
-    placement: 'right'
-});
 
-function addEvent() {
-    var eventHtml = $("#popup0");
-    eventHtml.show();   
-    eventHtml.attr("id","popup");
-    $(".container").append(eventHtml);
+
+window.eventManagement = {
+    add: function() {
+        var eventObject = {
+            id:window.eventList.length,
+            color:"red"
+        };
+        window.eventList.push(eventObject)
+        var eventHtml = $("#popup").clone();
+        eventHtml.show();
+        eventHtml.attr("id","popup"+eventObject.id);
+        this.setColor(eventObject.id);
+        $(".calendar").append(eventHtml);
+    },
+
+    setColor:function(eventId) {
+        if(eventId>window.eventList.length) return;
+        var eventObject = window.eventList[eventId];
+        var color = window.colorArray[eventObject.color].light;
+        $(".calendar").find('.head > button').attr("style","background-color:"+color+";");
+        $(".calendar").find('.content > .popup_container').attr("style","background-color:"+color+"!important;");
+    }
 }
--- js/globalVariables.js
+++ js/globalVariables.js
@@ -1,1 +1,8 @@
 window.eventList = new Array();
+
+window.colorArray = {
+	"red":{
+		"light":"#FF8080",
+		"dark":"#FF4D4D"
+	}
+}
