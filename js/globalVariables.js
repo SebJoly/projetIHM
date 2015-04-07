0bc989b59b8c90784e32a140cfc329b48872957a
Florian Malapel <flo.malap@hotmail.fr>
2015-04-07 15:12:34 +0200

    Some shit

--- js/globalVariables.js
+++ js/globalVariables.js
@@ -4,5 +4,25 @@ window.colorArray = {
 	"red":{
 		"light":"#FF8080",
 		"dark":"#FF4D4D"
+	},
+
+	"yellow":{
+		"light":"#F7FF4D",
+		"dark":"#EDFF1E"
+	},
+
+	"green":{
+		"light":"#4FFA63",
+		"dark":"#FF4D4D"
+	},
+
+	"blue":{
+		"light":"#7683FA",
+		"dark":"#1C32FA"
+	},
+
+	"purple":{
+		"light":"#FF50E9",
+		"dark":"#FF00DE"
 	}
 }
